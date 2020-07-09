5183ea6b25b0768d7686d6e4f887306a79ee6b9f
willian <wiliam-m-@hotmail.com>
2020-07-09 09:57:07 -0300

    final api

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,9 +1,11 @@
 from app import app
 from flask import request
-from numpy import fromstring, uint8, argmax
+from numpy import fromstring, uint8, argmax, array
 from base64 import b64decode, b64encode
 from cv2 import imdecode, IMREAD_COLOR, imencode
-from cv2.dnn import readNetFromDarknet, blobFromImage
+from cv2.dnn import readNetFromDarknet, blobFromImage, NMSBoxes
+from cv2 import rectangle, putText, FONT_HERSHEY_SIMPLEX, imshow, waitKey
+import jsonify
 
 
 @app.route('/api', methods=['POST', 'GET'])
@@ -31,11 +33,46 @@ def index():
                 for detection in output:
                     scores = detection[5:]
                     classID = argmax(scores)
-                    confidences = scores[classID]
+                    confidence = scores[classID]
 
-            return {'image': 'ok'}
+                    if confidence > 0.3:
+                        box = detection[0:4] * array([W, H, W, H])
+                        (centerX, centerY, width, height) = box.astype("int")
+
+                        x = int(centerX - (width / 2))
+                        y = int(centerY - (height / 2))
+
+                        boxes.append([x, y, int(width), int(height)])
+                        confidences.append(float(confidence))
+                        classIDs.append(classID)
+
+            idxs = NMSBoxes(boxes, confidences, 0.3, 0.1)
+            labels = open('app/yolo_cfg/display.names').read().strip().split('\n')
+            colors = [[0, 255, 0], [0, 0, 255]]
+
+            if len(idxs) > 0:
+                for i in idxs.flatten():
+                    (x, y) = (boxes[i][0], boxes[i][1])
+                    (w, h) = (boxes[i][2], boxes[i][3])
+
+                    color = [int(c) for c in colors[classIDs[i]]]
+                    rectangle(img, (x-2, y-2), (x+w, y+h+3), color, 1)
+                    text = '{}'.format(labels[classIDs[i]])
+                    acc = '{:.2f}%'.format(confidences[i]*100)
+                    rectangle(img, (x-2, y+62), (x+47, y+47), color, -1)
+                    putText(img, text, (x, y+60), FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+                    putText(img, acc, (x, y+77), FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+            _, pred = imencode('.jpg', img)
+            pred = pred.tobytes()
+            pred = b64encode(pred)
+            return {
+                'nomeArquivo': request.data['nomeArquivo'],
+                'image': pred.decode('utf-8'),
+                'pred': "True" if classID else "False"
+            }
 
         except NameError as erro:
-            return {'image': erro}
+            return {'image': "Imagem invalida!"}
 
-    return {'message': 'error!'}
+    return {'message': 'Darknet Api Vizentec'}
